368661b60a72978603363bcd7f62e024697a9af3
Saar Sayfan <saarsayfan@gmail.com>
2018-05-27 22:34:30 -0700

    Added Multiple Rooms Support
    https://github.com/Bloblblobl/rumble-react/issues/4

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         super(props);
         this.timer = null;
         this.state = {
-            rooms: ['room0'],
+            rooms: [],
             roomName: 'room0',
             messages: [['2-24-2018:2', 'SYSTEM', 'Not Logged In']]
         };
@@ -23,35 +23,48 @@ class App extends Component {
     onLogin = (authToken) => {
         this.setState({...this.state, authToken: authToken});
         this.getRooms();
-        this.joinRoom()
-            .then(this.timer = setInterval(() => this.fetchMessages(), 1000));
     };
 
     onRoomChange = (room) => {
+        console.log("On Room Change: " + room);
+        this.setState({...this.state, roomName: room});
     };
 
     getRooms = () => {
         const config = {headers: {Authorization: this.state.authToken}};
         return axios.get(`${url}/rooms`, config)
-            .then((data) => this.setState({...this.state, rooms: data.data.result}))
+            .then((data) => {
+                this.setState({...this.state, rooms: data.data.result});
+                this.joinRooms(data.data.result);
+            })
             .catch(e => console.log(e));
     };
 
-    joinRoom = () => {
+    joinRooms = (rooms) => {
         const config = {headers: {Authorization: this.state.authToken}};
-        return axios.post(`${url}/room_member/${this.state.roomName}`, {}, config)
+        console.log(rooms[0]);
+        axios.post(`${url}/room_member/${rooms[0]}`, {}, config)
             .then((data) => data)
-            .catch(e => console.log(e));
+            .catch(e => console.log(e))
+            .then(this.timer = setInterval(() => this.fetchMessages(), 1000));
+        for (let i = 1; i < this.state.rooms.length; i++)
+        {
+            axios.post(`${url}/room_member/${rooms[i]}`, {}, config)
+                .then((data) => data)
+                .catch(e => console.log(e));
+        }
     };
 
     fetchMessages = () => {
+        let room = this.state.roomName;
+        console.log("Fetch Messages: " + room);
         const config = {headers: {Authorization: this.state.authToken}};
         const end = new Date().toISOString();
         let start = new Date();
         start.setDate(start.getDate() - 10000);
         start = start.toISOString();
 
-        axios.get(`${url}/messages/${this.state.roomName}/${start}/${end}`, config)
+        axios.get(`${url}/messages/${room}/${start}/${end}`, config)
             .then((response) => {
                 this.setState({...this.state, messages: response.data.result})
             })
@@ -59,7 +72,9 @@ class App extends Component {
     };
 
     render() {
-        let roomList = <RoomList rooms={this.state.rooms} onRoomChange={this.onRoomChange}/>;
+        let roomList = <RoomList rooms={this.state.rooms}
+                                 onRoomChange={this.onRoomChange}
+                                 activeRoom={this.state.roomName}/>;
         return (
             <div className="App">
                 <Sidebar sidebar={roomList} docked={true} pullRight={true}>
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -2,8 +2,19 @@ import React from 'react'
 import './../App.css';
 
 const RoomList = (props) => {
-    let rooms = props.rooms.map((room) =>
-        <li key={room} onClick={props.onRoomChange(room)}>{room}</li>);
+    let rooms = props.rooms.map((room) => {
+        let style = {
+            fontWeight: room === props.activeRoom ? "bold" : "normal",
+            paddingRight: "10px",
+            paddingLeft: "10px"
+        };
+        return <li className="unselectable"
+                   key={room}
+                   style={style}
+                   onClick={() => {
+                       props.onRoomChange(room)
+                   }}>{room}</li>;
+    });
     return rooms;
 };
 
