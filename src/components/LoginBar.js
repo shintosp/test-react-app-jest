b431ac479225aa5eb7a783d42121e92a882d14a5
Saar Sayfan <saarsayfan@gmail.com>
2018-07-22 15:43:08 -0700

    Added Create Room and Logout to UI (Not functional)

--- src/components/LoginBar.js
+++ src/components/LoginBar.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react'
 import axios from 'axios'
-import { Button, Navbar } from 'react-bootstrap';
+import {Button, Navbar} from 'react-bootstrap';
 import './../App.css';
 
 class LoginBar extends Component {
     constructor(props) {
         super(props);
-        this.state = {username: '', password: '', nickname: '', loggedIn: false}
+        this.state = {
+            username: '',
+            password: '',
+            nickname: '',
+            newRoom: '',
+            loggedIn: false
+        }
     }
 
     handleChange = () => {
@@ -20,15 +26,31 @@ class LoginBar extends Component {
 
     handleLogin = (event) => {
         axios.post(this.props.url + '/active_user', {username: this.state.username, password: this.state.password})
-            .then((data) => {this.props.onLogin(data.data.user_auth)})
+            .then((data) => {
+                this.props.onLogin(data.data.user_auth)
+            })
             .then(this.setState({...this.state, loggedIn: true}))
             .catch(e => console.log(e));
         event.preventDefault();
     };
 
     handleRegister = (event) => {
-        axios.post(this.props.url + '/user', {username: this.state.username, password: this.state.password, handle: this.state.nickname})
+        axios.post(this.props.url + '/user', {
+            username: this.state.username,
+            password: this.state.password,
+            handle: this.state.nickname
+        })
             .then((data) => this.handleLogin(event))
+            .catch(e => console.log(e));
+        event.preventDefault();
+    };
+
+    handleCreateRoom = (event) => {
+        axios.post(this.props.url + '/active_user', {username: this.state.username, password: this.state.password})
+            .then((data) => {
+                this.props.onLogin(data.data.user_auth)
+            })
+            .then(this.setState({...this.state, loggedIn: true}))
             .catch(e => console.log(e));
         event.preventDefault();
     };
@@ -38,30 +60,46 @@ class LoginBar extends Component {
             <Navbar className="App-header">
                 <Navbar.Header>
                     <img className="App-logo" width={80} height={40} src="/images/logo.png" alt="logo"/>
-                    <Navbar.Toggle />
+                    <Navbar.Toggle/>
                 </Navbar.Header>
-                {this.state.loggedIn ? "" :
-                <Navbar.Collapse>
-                    <Navbar.Form pullLeft>
-                        <label>
-                            Username:
-                            <input name="username" type="text" value={this.state.username}
-                                   onChange={this.handleChange()}/>
-                        </label>
-                        <label>
-                            Password:
-                            <input name="password" type="password" value={this.state.password}
-                                   onChange={this.handleChange()}/>
-                        </label>
-                        <label>
-                            Nickname:
-                            <input name="nickname" type="text" value={this.state.nickname}
-                                   onChange={this.handleChange()}/>
-                        </label>
-                        <Button className="App-Button" bsSize="small" type="submit" onClick={this.handleLogin}>Login</Button>
-                        <Button className="App-Button" bsSize="small" type="submit" onClick={this.handleRegister}>Register</Button>
-                    </Navbar.Form>
-                </Navbar.Collapse>}
+                {this.state.loggedIn ?
+                    <Navbar.Collapse>
+                        <Navbar.Form pullLeft>
+                            <label>
+                                New Room:
+                                <input name="newroom" type="text" value={this.state.newRoom}
+                                       onChange={this.handleChange()}/>
+                            </label>
+                            <Button className="App-Button" bsSize="small" type="submit"
+                                    onClick={this.handleCreateRoom}>Create Room</Button>
+                            <Button className="App-Button" bsSize="small" type="submit"
+                                    onClick={this.props.logout}>Logout</Button>
+                        </Navbar.Form>
+                    </Navbar.Collapse>
+                    :
+                    <Navbar.Collapse>
+                        <Navbar.Form pullLeft>
+                            <label>
+                                Username:
+                                <input name="username" type="text" value={this.state.username}
+                                       onChange={this.handleChange()}/>
+                            </label>
+                            <label>
+                                Password:
+                                <input name="password" type="password" value={this.state.password}
+                                       onChange={this.handleChange()}/>
+                            </label>
+                            <label>
+                                Nickname:
+                                <input name="nickname" type="text" value={this.state.nickname}
+                                       onChange={this.handleChange()}/>
+                            </label>
+                            <Button className="App-Button" bsSize="small" type="submit"
+                                    onClick={this.handleLogin}>Login</Button>
+                            <Button className="App-Button" bsSize="small" type="submit"
+                                    onClick={this.handleRegister}>Register</Button>
+                        </Navbar.Form>
+                    </Navbar.Collapse>}
             </Navbar>
         )
     }
